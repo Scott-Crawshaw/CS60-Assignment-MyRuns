70ad7e01d8a330a4cd2b644be5d5a6f822da41af
Scott Crawshaw <scott.crawshaw@gmail.com>
2020-02-03 11:20:05 -0500

    begin myruns3

---
+++ app/src/main/java/com/example/myruns/ExerciseEntry.java
@@ -0,0 +1,141 @@
+package com.example.myruns;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class ExerciseEntry {
+    private Long id;
+    private int inputType;  // Manual, GPS or automatic
+    private int activityType;     // Running, cycling etc.
+    private Calendar dateTime;    // When does this entry happen
+    private int duration;         // Exercise duration in seconds
+    private float distance;      // Distance traveled. Either in meters or feet.
+    private float avgPace;       // Average pace
+    private float avgSpeed;     // Average speed
+    private int calorie;        // Calories burnt
+    private float climb;         // Climb. Either in meters or feet.
+    private int heartRate;       // Heart rate
+    private int privacy;
+    private String comment;       // Comments
+    private ArrayList<LatLng> locationList; // Location list
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getInputType() {
+        return inputType;
+    }
+
+    public void setInputType(int inputType) {
+        this.inputType = inputType;
+    }
+
+    public int getActivityType() {
+        return activityType;
+    }
+
+    public void setActivityType(int activityType) {
+        this.activityType = activityType;
+    }
+
+    public Calendar getDateTime() {
+        return dateTime;
+    }
+
+    public String getDateTimeSQL() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(dateTime.getTime());
+    }
+
+
+    public void setDateTime(Calendar dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public float getAvgPace() {
+        return avgPace;
+    }
+
+    public void setAvgPace(float avgPace) {
+        this.avgPace = avgPace;
+    }
+
+    public float getAvgSpeed() {
+        return avgSpeed;
+    }
+
+    public void setAvgSpeed(float avgSpeed) {
+        this.avgSpeed = avgSpeed;
+    }
+
+    public int getCalorie() {
+        return calorie;
+    }
+
+    public void setCalorie(int calorie) {
+        this.calorie = calorie;
+    }
+
+    public float getClimb() {
+        return climb;
+    }
+
+    public void setClimb(float climb) {
+        this.climb = climb;
+    }
+
+    public int getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(int heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public ArrayList<LatLng> getLocationList() {
+        return locationList;
+    }
+
+    public void setLocationList(ArrayList<LatLng> locationList) {
+        this.locationList = locationList;
+    }
+
+    public int getPrivacy() {
+        return privacy;
+    }
+
+    public void setPrivacy(int privacy) {
+        this.privacy = privacy;
+    }
+}
---
+++ app/src/main/java/com/example/myruns/ExerciseEntryDbHelper.java
@@ -0,0 +1,85 @@
+package com.example.myruns;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class ExerciseEntryDbHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_EXERCISE = "exercise";
+    private static final String DATABASE_NAME = "exercise.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private final String CREATE_TABLE_ENTRIES = "CREATE TABLE IF NOT EXISTS "+TABLE_EXERCISE+" ("+
+            "_id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+            "input_type INTEGER NOT NULL,"+
+            "activity_type INTEGER NOT NULL,"+
+            "date_time DATETIME NOT NULL,"+
+            "duration INTEGER NOT NULL,"+
+            "distance REAL,"+
+            "avg_pace REAL,"+
+            "avg_speed REAL,"+
+            "calories INTEGER,"+
+            "climb REAL,"+
+            "heartrate INTEGER,"+
+            "comment TEXT,"+
+            "privacy INTEGER,"+
+            "gps_data BLOB );";
+
+    public ExerciseEntryDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EXERCISE);
+        onCreate(db);
+    }
+
+    // Insert a item given each column value
+    public long insertEntry(ExerciseEntry entry) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("input_type", entry.getInputType());
+        contentValues.put("activity_type", entry.getActivityType());
+        contentValues.put("date_time", entry.getDateTimeSQL());
+        contentValues.put("duration", entry.getDuration());
+        contentValues.put("distance", entry.getDistance());
+        contentValues.put("avg_pace", entry.getAvgPace());
+        contentValues.put("avg_speed", entry.getAvgSpeed());
+        contentValues.put("calories", entry.getCalorie());
+        contentValues.put("climb", entry.getClimb());
+        contentValues.put("heartrate", entry.getHeartRate());
+        contentValues.put("comment", entry.getComment());
+        contentValues.put("privacy", entry.getPrivacy());
+        contentValues.put("gps_data", new byte[0]);
+
+        long rowID = db.insert(TABLE_EXERCISE, null, contentValues);
+        db.close();
+        return rowID;
+    }
+
+    // Remove an entry by giving its index
+    public void removeEntry(long rowIndex) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(TABLE_EXERCISE, "_id =" + rowIndex, null);
+        db.close();
+    }
+
+    // Query a specific entry by its index.
+    public ExerciseEntry fetchEntryByIndex(long rowId) {
+    }
+
+    // Query the entire table, return all rows
+    public ArrayList<ExerciseEntry> fetchEntries() {
+    }
+
+}
