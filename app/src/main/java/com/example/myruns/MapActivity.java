90ebcfe7169ecf0313eb95cdfd0245d4e8d65647
Scott Crawshaw <scott.crawshaw@gmail.com>
2020-02-10 17:11:54 -0500

    map loads

--- app/src/main/java/com/example/myruns/MapActivity.java
+++ app/src/main/java/com/example/myruns/MapActivity.java
@@ -1,20 +1,157 @@
 package com.example.myruns;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener,
+        GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener{
+
+    boolean start = false;
+    GoogleMap map;
+    PolylineOptions polylineOptions;
+    LocationManager locationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         setTitle("Map");
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        polylineOptions = new PolylineOptions();
+        polylineOptions.color(Color.BLUE);
+
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+        if(locationManager != null)
+            locationManager.removeUpdates(this);
     }
 
     public void killActivity(View v){
         finish();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location == null){
+            return;
+        }
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        LatLng latLng = new LatLng(lat, lng);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        map.addMarker(markerOptions);
+        polylineOptions.add(latLng);
+        map.addPolyline(polylineOptions);
+
+        if(!start){
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+            map.animateCamera(cameraUpdate);
+            start = true;
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.setOnMapClickListener(this);
+        map.setOnMapLongClickListener(this);
+
+        checkPermission();
+    }
+
+    public void checkPermission(){
+        if(Build.VERSION.SDK_INT < 23) {
+            return;
+        }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    0);
+        }
+        else {
+            initLocationManager();
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initLocationManager();
+            }
+        }
+    }
+
+    public void initLocationManager(){
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            String provider = locationManager.getBestProvider(criteria, true);
+            Location location = locationManager.getLastKnownLocation(provider);
+            onLocationChanged(location);
+            locationManager.requestLocationUpdates(provider, 0, 0, this);
+        }catch (SecurityException e){}
+    }
+
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
--- app/src/main/java/com/example/myruns/OptionsAdapter.java
+++ app/src/main/java/com/example/myruns/OptionsAdapter.java
@@ -8,7 +8,6 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.text.InputType;
 import android.text.format.DateFormat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
